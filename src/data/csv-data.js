36b9c1eccaa1a7e542a79696a4db7c64e7bb1e37
anishdalvi <anishdalvi01@gmail.com>
2025-01-19 03:28:59 +0530

    Basic Table with Sample Data

---
+++ src/data/csv-data.js
@@ -0,0 +1,410 @@
+export const csvData = [
+  {
+    "Main Topic": "Java Basics",
+    Subtopics: [
+      {
+        Subtopic: "Programming Language and Memory Management",
+        Details: [
+          {
+            Detail: "What is a programming Language",
+          },
+          {
+            Detail: "Machine Language",
+          },
+          {
+            Detail: "Assembly Language",
+          },
+          {
+            Detail: "High Level Language",
+          },
+          {
+            Detail: "Procedural Language",
+          },
+          {
+            Detail: "Functional Language",
+          },
+          {
+            Detail: "Object Oriented Language",
+          },
+          {
+            Detail: "Scripting Language",
+          },
+          {
+            Detail: "Stack Memory",
+          },
+          {
+            Detail: "Heap Memory",
+          },
+          {
+            Detail: "Garbage Collection",
+          },
+        ],
+        "Video Link": "https://youtu.be/DEC268j2hk0",
+      },
+      {
+        Subtopic: "Flow of a program",
+        Details: [
+          {
+            Detail: "Flowchart",
+          },
+          {
+            Detail: "Psuedocode",
+          },
+          {
+            Detail: "Algorithm",
+          },
+        ],
+        "Video Link": "https://youtu.be/wL6sbJOzF5A",
+      },
+      {
+        Subtopic: "How Java Program Compiles",
+        Details: [
+          {
+            Detail: "Byte code",
+          },
+          {
+            Detail: "Machine code",
+          },
+          {
+            Detail: "why java is platform independent",
+          },
+          {
+            Detail: "JDK, JRE, JVM, JIT",
+          },
+        ],
+        "Video Link": "https://youtu.be/2Uer9X75EPI",
+      },
+      {
+        Subtopic: "Writing your first program",
+        Details: [
+          {
+            Detail: "installation of Java",
+          },
+          {
+            Detail: "installation of IDE",
+          },
+          {
+            Detail: '"Hello Coder"',
+          },
+          {
+            Detail: "entrypoint of program",
+          },
+          {
+            Detail: "commandline arguments",
+          },
+          {
+            Detail: "generating byte code in style",
+          },
+          {
+            Detail: "How computer runs your program?",
+          },
+          {
+            Detail: "writing comments in java",
+          },
+        ],
+        "Video Link": "https://youtu.be/1Z2QzcbLicY",
+      },
+      {
+        Subtopic: "Java being Java",
+        Details: [
+          {
+            Detail: "Variables and constants",
+          },
+          {
+            Detail: "Literals and keywords",
+          },
+          {
+            Detail: "data types",
+          },
+          {
+            Detail: "Program to swap two numbers",
+          },
+          {
+            Detail: "Type casting Implicit and explicit",
+          },
+          {
+            Detail: "Automatic type promotion",
+          },
+          {
+            Detail: "Wrapper class",
+          },
+          {
+            Detail: "ASCII Values",
+          },
+          {
+            Detail: "Unicode style",
+          },
+        ],
+        "Video Link": "https://youtu.be/FVs7Z9RfIJE",
+      },
+      {
+        Subtopic: "Inputs and Operators",
+        Details: [
+          {
+            Detail: "Operators in java",
+          },
+          {
+            Detail: "How to take input",
+          },
+        ],
+        "Video Link": "https://youtu.be/NZF0Rwrubs0",
+      },
+      {
+        Subtopic: "Organize your code",
+        Details: [
+          {
+            Detail: "Java Packages",
+          },
+          {
+            Detail: "Access Specifiers",
+          },
+        ],
+        "Video Link": "https://youtu.be/Axw8usR3rEA",
+      },
+      {
+        Subtopic: "Control Statements",
+        Details: [
+          {
+            Detail: "if-else",
+          },
+          {
+            Detail: "switch case",
+          },
+          {
+            Detail: "while loop",
+          },
+          {
+            Detail: "do-while Loop",
+          },
+          {
+            Detail: "for each loop",
+          },
+        ],
+        "Video Link": "https://youtu.be/N6unLhAxy8s",
+      },
+      {
+        Subtopic: "Functions in Java",
+        Details: [
+          {
+            Detail: "Functions",
+          },
+          {
+            Detail: "Types of functions",
+          },
+          {
+            Detail: "create your own functions",
+          },
+          {
+            Detail: "call by value and call by reference",
+          },
+        ],
+        "Video Link": "https://youtu.be/IkOkAzNuefc",
+      },
+    ],
+  },
+  {
+    "Main Topic": "Searching Algorithms",
+    Subtopics: [
+      {
+        Subtopic: "Searching Algorithms - 1",
+        Details: [
+          {
+            Detail: "Linear Search",
+          },
+          {
+            Detail: "Q. search an Element / first occurrence",
+          },
+          {
+            Detail: "Q. Find the minimum element in an array (1D/2D)",
+          },
+          {
+            Detail: "Q. Find the maximum element in an array (1D/2D)",
+          },
+          {
+            Detail: "Q. Find max sum 1D array in a 2D array",
+          },
+          {
+            Detail: "Q. Search all occurrences (1D/2D)",
+          },
+          {
+            Detail: "Q. Search last occurrence (1D/2D)",
+          },
+        ],
+        "Video Link": "https://youtu.be/slqFdKVMjeQ",
+      },
+      {
+        Subtopic: "Searching Algorithms - 2",
+        Details: [
+          {
+            Detail: "Binary Search",
+          },
+          {
+            Detail: "Sorted in increasing",
+          },
+          {
+            Detail: "Sorted in decreasing",
+          },
+          {
+            Detail: "Order Agnostic Binary Search",
+          },
+        ],
+        "Video Link": "https://youtu.be/kOIECDEUzaI",
+      },
+      {
+        Subtopic: "Questions on Binary Search Part 1",
+        Details: [
+          {
+            Detail: "Q1. Ceiling of a Number",
+          },
+          {
+            Detail: "Q2. Floor of a Number",
+          },
+          {
+            Detail: "Q3. First and Last Position of element in sorted array",
+          },
+          {
+            Detail: "Q4. Count all occurrence of element in an array",
+          },
+          {
+            Detail: "Q5. Next smallest letter in sorted array",
+          },
+        ],
+        "Video Link": "https://youtu.be/P-vl_BrdxJA",
+      },
+      {
+        Subtopic: "Questions on Binary Search Part 2",
+        Details: [
+          {
+            Detail: "Binary Search in range",
+          },
+          {
+            Detail: "Q6. Find Minimum Difference in a Sorted array.",
+          },
+          {
+            Detail: "Q7. Find an element in an array of Infinite size",
+          },
+          {
+            Detail:
+              "Q8. Find first 1 in an infinite and sorted array of 0s and 1s",
+          },
+        ],
+        "Video Link": "https://youtu.be/DoVawmdh5NM",
+      },
+      {
+        Subtopic: "Questions on Binary Search Part 3",
+        Details: [
+          {
+            Detail: "Bitonic Array",
+          },
+          {
+            Detail: "Q9. Find pivot in bitonic array",
+          },
+          {
+            Detail: "Q10. search in bitonic array",
+          },
+        ],
+        "Video Link": "https://youtu.be/IK5eSvNw9Qw",
+      },
+      {
+        Subtopic: "Questions on Binary Search Part 4",
+        Details: [
+          {
+            Detail: "Sorted and Rotated Array (Clockwise and Anticlockwise)",
+          },
+          {
+            Detail: "Q11. count number of rotations",
+          },
+          {
+            Detail:
+              "Q12. search in rotated sorted array ( with and without duplicates)",
+          },
+          {
+            Detail:
+              "Q13 Find min / max (peak) element in rotated and sorted array",
+          },
+        ],
+        "Video Link": "https://youtu.be/n12QcCu8oBI",
+      },
+      {
+        Subtopic: "Questions on Binary Search Part 5",
+        Details: [
+          {
+            Detail: "Q14. Allocated minimum number of pages",
+          },
+          {
+            Detail: "Q15. Capacity to Ship within D Days",
+          },
+        ],
+        "Video Link": "https://youtu.be/_4LmUWmmYbY",
+      },
+      {
+        Subtopic: "Q16. Koko Eating bananas",
+        Details: [],
+        "Video Link": "https://youtu.be/BUFHoQIOnAs",
+      },
+      {
+        Subtopic: "Q17. Smallest Divisor given a threshold",
+        Details: [],
+        "Video Link": "https://youtu.be/pSGtbhpuhbk",
+      },
+      {
+        Subtopic: "Q18. Minimize Maximum of products distributed to any store",
+        Details: [],
+        "Video Link": "https://youtu.be/4O6wrTtUQvY",
+      },
+      {
+        Subtopic: "Q19. Aggressive cows",
+        Details: [],
+        "Video Link": "https://youtu.be/86xSPxfc4iQ",
+      },
+      {
+        Subtopic: "Q20. Median of two sorted arrays",
+        Details: [],
+        "Video Link": "https://youtu.be/2BOgAlmyTkc",
+      },
+      {
+        Subtopic: "Q21. Kth element of two sorted arrays",
+        Details: [],
+        "Video Link": "https://youtu.be/SB6j8D95eHM",
+      },
+      {
+        Subtopic: "Questions on Binary Search Part 6",
+        Details: [
+          {
+            Detail: "Q22. Single Element in a Sorted Array ",
+          },
+          {
+            Detail: "Q23. Finding square root ",
+          },
+          {
+            Detail: "Q24. Count Squares ",
+          },
+        ],
+        "Video Link": "https://youtu.be/f09DayNeigg",
+      },
+      {
+        Subtopic: "Other Searching Algorithms",
+        Details: [
+          {
+            Detail: "Jump Search",
+          },
+          {
+            Detail: "Interpolation Search",
+          },
+          {
+            Detail: "Exponential Search",
+          },
+          {
+            Detail: "Ternary Search",
+          },
+        ],
+        "Video Link": "https://youtu.be/rFwBm-VT99A",
+      },
+      {
+        Subtopic: "Q. Median of a Sorted Matrix",
+        Details: [],
+        "Video Link": "https://youtu.be/aNIfGl1ZKQE",
+      },
+    ],
+  },
+];
