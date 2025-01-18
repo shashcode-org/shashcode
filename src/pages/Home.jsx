60c788df0123cb1f8fccdbcce12563c0f9946225
anishdalvi <anishdalvi01@gmail.com>
2025-01-19 00:48:12 +0530

    Tailwind Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1 className='text-2xl font-bold'>Welcome to Home Page</h1>
+    </div>
   )
 }
 
