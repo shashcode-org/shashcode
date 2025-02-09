69ae68788cf82c3c525739645512e1704e09cda1
anishdalvi <anishdalvi01@gmail.com>
2025-02-09 20:52:39 +0530

    Added 2 More UIs Separate Questions Col

---
+++ src/data/csv-data-v2.js
@@ -0,0 +1,335 @@
+export const csvDataAll = [
+  {
+    "Main Topic": "Java Basics",
+    Subtopics: [
+      {
+        Subtopic: "Programming Language and Memory Management",
+        Details: [
+          {
+            Detail: "What is a programming Language",
+          },
+          {
+            Detail: "Machine Language",
+          },
+          {
+            Detail: "Assembly Language",
+          },
+          {
+            Detail: "High Level Language",
+          },
+          {
+            Detail: "Procedural Language",
+          },
+          {
+            Detail: "Functional Language",
+          },
+          {
+            Detail: "Object Oriented Language",
+          },
+          {
+            Detail: "Scripting Language",
+          },
+          {
+            Detail: "Stack Memory",
+          },
+          {
+            Detail: "Heap Memory",
+          },
+          {
+            Detail: "Garbage Collection",
+          },
+        ],
+        Questions: [],
+        "Video Link": "https://youtu.be/DEC268j2hk0",
+      },
+      {
+        Subtopic: "Flow of a program",
+        Details: [
+          {
+            Detail: "Flowchart",
+          },
+          {
+            Detail: "Psuedocode",
+          },
+          {
+            Detail: "Algorithm",
+          },
+        ],
+        Questions: [],
+        "Video Link": "https://youtu.be/wL6sbJOzF5A",
+      },
+    ],
+  },
+  {
+    "Main Topic": "Searching Algorithms",
+    Subtopics: [
+      {
+        Subtopic: "Searching Algorithms - 1",
+        Details: [
+          {
+            Detail: "Linear Search",
+          },
+        ],
+        Questions: [
+          {
+            Question: "Q. search an Element / first occurrence",
+            "Video Link": "https://youtu.be/slqFdKVMjeQ",
+          },
+          {
+            Question: "Q. Find the minimum element in an array (1D/2D)",
+            "Video Link": null,
+          },
+          {
+            Question: "Q. Find the maximum element in an array (1D/2D)",
+            "Video Link": null,
+          },
+          {
+            Question: "Q. Find max sum 1D array in a 2D array",
+            "Video Link": null,
+          },
+          {
+            Question: "Q. Search all occurrences (1D/2D)",
+            "Video Link": null,
+          },
+          {
+            Question: "Q. Search last occurrence (1D/2D)",
+            "Video Link": null,
+          },
+        ],
+        "Video Link": "https://youtu.be/slqFdKVMjeQ",
+      },
+      {
+        Subtopic: "Searching Algorithms - 2",
+        Details: [
+          {
+            Detail: "Binary Search",
+          },
+          {
+            Detail: "Sorted in increasing",
+          },
+          {
+            Detail: "Sorted in decreasing",
+          },
+          {
+            Detail: "Order Agnostic Binary Search",
+          },
+        ],
+        Questions: [],
+        "Video Link": "https://youtu.be/kOIECDEUzaI",
+      },
+      {
+        Subtopic: "Questions on Binary Search Part 1",
+        Details: [],
+        Questions: [
+          {
+            Question: "Q1. Ceiling of a Number",
+            "Video Link": "https://youtu.be/P-vl_BrdxJA",
+          },
+          {
+            Question: "Q2. Floor of a Number",
+            "Video Link": null,
+          },
+          {
+            Question: "Q3. First and Last Position of element in sorted array",
+            "Video Link": null,
+          },
+          {
+            Question: "Q4. Count all occurrence of element in an array",
+            "Video Link": null,
+          },
+          {
+            Question: "Q5. Next smallest letter in sorted array",
+            "Video Link": null,
+          },
+        ],
+        "Video Link": "https://youtu.be/P-vl_BrdxJA",
+      },
+      {
+        Subtopic: "Questions on Binary Search Part 5",
+        Details: [],
+        Questions: [
+          {
+            Question: "Q14. Allocated minimum number of pages",
+            "Video Link": "https://youtu.be/_4LmUWmmYbY",
+          },
+          {
+            Question: "Q15. Capacity to Ship within D Days",
+            "Video Link": null,
+          },
+          {
+            Question: "Q16. Koko Eating bananas",
+            "Video Link": "https://youtu.be/BUFHoQIOnAs",
+          },
+          {
+            Question: "Q17. Smallest Divisor given a threshold",
+            "Video Link": "https://youtu.be/pSGtbhpuhbk",
+          },
+          {
+            Question:
+              "Q18. Minimize Maximum of products distributed to any store",
+            "Video Link": "https://youtu.be/4O6wrTtUQvY",
+          },
+          {
+            Question: "Q19. Aggressive cows",
+            "Video Link": "https://youtu.be/86xSPxfc4iQ",
+          },
+          {
+            Question: "Q20. Median of two sorted arrays",
+            "Video Link": "https://youtu.be/2BOgAlmyTkc",
+          },
+          {
+            Question: "Q21. Kth element of two sorted arrays",
+            "Video Link": "https://youtu.be/SB6j8D95eHM",
+          },
+        ],
+        "Video Link": "https://youtu.be/_4LmUWmmYbY",
+      },
+      {
+        Subtopic: "Questions on Binary Search Part 6",
+        Details: [],
+        Questions: [
+          {
+            Question: "Q22. Single Element in a Sorted Array ",
+            "Video Link": "https://youtu.be/f09DayNeigg",
+          },
+          {
+            Question: "Q23. Finding square root ",
+            "Video Link": null,
+          },
+          {
+            Question: "Q24. Count Squares ",
+            "Video Link": null,
+          },
+        ],
+        "Video Link": "https://youtu.be/f09DayNeigg",
+      },
+      {
+        Subtopic: "Other Searching Algorithms",
+        Details: [
+          {
+            Detail: "Jump Search",
+          },
+          {
+            Detail: "Interpolation Search",
+          },
+          {
+            Detail: "Exponential Search",
+          },
+          {
+            Detail: "Ternary Search",
+          },
+          {
+            Detail: "The Sorting",
+          },
+          {
+            Detail: "The Element 2.1",
+          },
+          {
+            Detail: "The Element 2.2",
+          },
+          {
+            Detail: "The Element 3",
+          },
+          {
+            Detail: "The Element 4",
+          },
+          {
+            Detail: "The Element 5",
+          },
+          {
+            Detail: "The Element 6",
+          },
+          {
+            Detail: "The Element 7",
+          },
+          {
+            Detail: "The Element 8",
+          },
+        ],
+        Questions: [
+          {
+            Question: "Q. Median of a Sorted Matrix",
+            "Video Link": "https://youtu.be/aNIfGl1ZKQE",
+          },
+          {
+            Question: "Q. Kth Smallest element of a Matrix",
+            "Video Link": "https://youtu.be/HuOcDlB1uXk",
+          },
+          {
+            Question: "Q25. Koko Eating bananas",
+            "Video Link": "https://youtu.be/BUFHoQIOnAs",
+          },
+          {
+            Question: "Q26. Smallest Divisor given a threshold",
+            "Video Link": "https://youtu.be/pSGtbhpuhbk",
+          },
+          {
+            Question:
+              "Q27. Minimize Maximum of products distributed to any store",
+            "Video Link": "https://youtu.be/4O6wrTtUQvY",
+          },
+          {
+            Question: "Q28. Aggressive cows",
+            "Video Link": "https://youtu.be/86xSPxfc4iQ",
+          },
+          {
+            Question: "Q29. Median of two sorted arrays",
+            "Video Link": "https://youtu.be/2BOgAlmyTkc",
+          },
+          {
+            Question: "Q30. Kth element of two sorted arrays",
+            "Video Link": "https://youtu.be/SB6j8D95eHM",
+          },
+        ],
+        "Video Link": "https://youtu.be/rFwBm-VT99A",
+      },
+      {
+        Subtopic: "Questions on Binary Search Part 7",
+        Details: [
+          {
+            Detail: "The Element 9",
+          },
+          {
+            Detail: "The Element 10.1",
+          },
+          {
+            Detail: "The Element 10.2",
+          },
+          {
+            Detail: "The Element 10.3",
+          },
+          {
+            Detail: "The Element 11",
+          },
+        ],
+        Questions: [
+          {
+            Question: "Q31. Single Element in a Sorted Array ",
+            "Video Link": "https://youtu.be/f09DayNeigg",
+          },
+          {
+            Question: "Q32. Finding square root ",
+            "Video Link": null,
+          },
+          {
+            Question: "Q33. Count Squares ",
+            "Video Link": null,
+          },
+          {
+            Question: "Q34. Median of a Sorted Matrix",
+            "Video Link": "https://youtu.be/aNIfGl1ZKQE",
+          },
+          {
+            Question: "Q.35 Kth Smallest element of a Matrix",
+            "Video Link": "https://youtu.be/HuOcDlB1uXk",
+          },
+          {
+            Question: "Q36. Aggressive cows",
+            "Video Link": "https://youtu.be/86xSPxfc4iQ",
+          },
+        ],
+        "Video Link": "https://youtu.be/f09DayNeigg",
+      },
+    ],
+  },
+];
--- src/pages/DSA.jsx
+++ src/pages/DSA.jsx
@@ -4,19 +4,39 @@ import {
     Route,
     Outlet, Link
 } from 'react-router-dom'
-import { CSV_UI_1, CSV_UI_1_All, CSV_UI_2, CSV_UI_2_All,  } from '../components'
+import { CSV_UI_1, CSV_UI_1_All, CSV_UI_2, CSV_UI_2_All, CSV_UI_V2_Data_1, CSV_UI_V2_Data_2 } from '../components'
+import { useNavigate } from "react-router-dom";
+
 
 const DSA = () => {
+
+    const navigate = useNavigate();
+
+    const handleMainTopicChange = (e) => {
+        const selectedValue = e.target.value;
+        if (selectedValue) {
+            navigate(selectedValue);
+        }
+    };
    
     return (
         <div className='my-10'>
             <h1 className='text-3xl font-semibold uppercase flex justify-between items-center'>
                 DSA
                 <div className='flex gap-5 text-xl'>
-                    <Link to='/dsa/csv_ui_1' className='text-blue-500'>UI 1</Link>
-                    <Link to='/dsa/csv_ui_2' className='text-purple-500'>UI 2</Link>
-                    <Link to='/dsa/csv_ui_1_all' className='text-blue-500'>UI 1 All</Link>
-                    <Link to='/dsa/csv_ui_2_all' className='text-purple-500'>UI 2 All</Link>
+                    <select
+                        id="mainTopic"
+                        className="border border-gray-300 px-2 py-1"
+                        onChange={handleMainTopicChange}
+                    >
+                        <option value="">-- Select --</option>
+                        <option value="/dsa/csv_ui_1">UI 1</option>
+                        <option value="/dsa/csv_ui_2">UI 2</option>
+                        <option value="/dsa/csv_ui_1_all">UI 1 All</option>
+                        <option value="/dsa/csv_ui_2_all">UI 2 All</option>
+                        <option value="/dsa/csv_ui_v2_data_1">UI V2 Data 1(Details/Questions Separated)</option>
+                        <option value="/dsa/csv_ui_v2_data_2">UI V2 Data 2(Details/Questions Combined)</option>
+                    </select>
                 </div>
             </h1>
             
@@ -27,6 +47,8 @@ const DSA = () => {
                 <Route path="/csv_ui_2" element={<CSV_UI_2 />} />
                 <Route path="/csv_ui_1_all" element={<CSV_UI_1_All />} />
                 <Route path="/csv_ui_2_all" element={<CSV_UI_2_All />} />
+                <Route path="/csv_ui_v2_data_1" element={<CSV_UI_V2_Data_1 />} />
+                <Route path="/csv_ui_v2_data_2" element={<CSV_UI_V2_Data_2 />} />
             </Routes>
         </div>
     )
