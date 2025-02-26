ae37d3a45823cec786f9fb7c74b911c36ac123f1
anishdalvi <anishdalvi01@gmail.com>
2025-02-26 21:09:56 +0530

    edited home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='mt-10'>
-      <h1 className='text-3xl font-semibold uppercase'>Welcome to Home Page</h1>
+      <h1 className='text-3xl font-semibold uppercase'> Welcome to Home Page of Shash Code </h1>
     </div>
   )
 }
