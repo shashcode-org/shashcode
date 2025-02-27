49151c9af33e0de07873be8e07d7b41f2e60dbd4
anishdalvi <anishdalvi01@gmail.com>
2025-02-27 23:02:00 +0530

    Removed Colors from About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import { ColorTest } from '../components'
 
 const About = () => {
   return (
     <div className='mt-10'>
       <h1 className='text-3xl font-semibold uppercase'> About </h1>
-
-      <div className='mx-auto mt-10 bg-vite_dark p-10 text-vite_light flex flex-col gap-10 rounded-2xl'>
-        <h1 className='text-3xl font-semibold uppercase border-b pb-5'>Colors of Site</h1>
-        <ColorTest />    
-      </div>
+      <p className='text-lg'>This is the about page</p>
     </div>
   )
 }
