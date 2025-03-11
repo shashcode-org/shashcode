ea2476e3ff7e645248e3f9cb613d1a9e8a5693fd
anishdalvi <anishdalvi01@gmail.com>
2025-03-12 00:12:13 +0530

    Edit DSA Code for Margin and Padding

--- src/pages/DSA.jsx
+++ src/pages/DSA.jsx
@@ -4,10 +4,10 @@ import { CSV_TABLE_UI } from '../components'
 
 const DSA = () => {
 
-   
     return (
+        
         <div className='my-10'>
-            
+
             <h1 className='text-3xl font-semibold uppercase flex justify-between items-center'>
                 DSA
             </h1>
