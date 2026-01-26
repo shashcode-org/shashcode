532f9c57f0235cf762ab14632821048eac7250fc
shashwat <shashwattiwari8@gmail.com>
2026-01-27 00:49:19 +0530

    revert republic day

--- src/App.tsx
+++ src/App.tsx
@@ -14,37 +14,35 @@ import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import ScrollToTop from "./components/ScrollToTop";
 import "./App.css";
-import IndependenceToast from "@/components/RepublicToast";
 
 const queryClient = new QueryClient();
 
 const App = () => {
 
   return (
-  
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      {/* 🇮🇳 Republic Day toast (temporary) */}
-        <IndependenceToast />
-      <BrowserRouter>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dsa" element={<DSA />} />
-          <Route path="/java-dsa" element={<JavaDSA />} />
-          {/* <Route path="/last-minute-dsa" element={<LastMinuteDSA />} /> */}
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="/terms" element={<Terms />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-          <Route path="/about" element={<About />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-)};
+
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dsa" element={<DSA />} />
+            <Route path="/java-dsa" element={<JavaDSA />} />
+            {/* <Route path="/last-minute-dsa" element={<LastMinuteDSA />} /> */}
+            <Route path="/privacy" element={<Privacy />} />
+            <Route path="/terms" element={<Terms />} />
+            <Route path="/contact-us" element={<ContactUs />} />
+            <Route path="/about" element={<About />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  )
+};
 
 export default App;
