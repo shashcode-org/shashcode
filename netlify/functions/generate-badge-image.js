6b205126116d46512727e06fec5658ca2e417e1a
shashwat <shashwattiwari8@gmail.com>
2026-04-10 01:02:05 +0530

    added state

--- netlify/functions/generate-badge-image.js
+++ netlify/functions/generate-badge-image.js
@@ -7,7 +7,7 @@ import sharp from "sharp";
 
 // Initialize Supabase
 const supabase = createClient(
-    process.env.VITE_SUPABASE_URL,
+    process.env.SUPABASE_URL,
     process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
